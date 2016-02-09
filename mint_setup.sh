8b6d5670ec6595fc2da5ea07d1cebc270a72d260
treystaff <treystaff@gmail.com>
2016-02-09 16:09:28 -0600

    Added support for i3 configuration, version control/automatic configuration of vim/bash/i3 configs etc.; added installation of some useful packages.

--- mint_setup.sh
+++ mint_setup.sh
@@ -17,8 +17,26 @@ case $yn in
 
 read -p "Is installation on an SSD?"
 
-# Install guake
-apt-get install -y guake
+# Install guake (obsolete if we are installing i3....)
+#apt-get install -y guake
+
+# WRITE LATEX. LOVE LATEX. SURRENDER.
+apt-get install texmaker texlive
+
+# Install the i3 window manager
+apt-get install -y i3 i3status dmenu
+
+# Install vim!
+apt-get install -y vim
+
+# Install feh (for background stuff...)
+apt-get install -y feh
+
+# Install scrot for screen capture
+apt-get install -y scrot
+
+# Install the xautolock utility for screen locking in X
+apt-get install -y xautolock
 
 # Imagemagick is useful for PIL
 apt-get install -y imagemagick
@@ -30,6 +48,14 @@ apt-get install -y qgis qgis-python
 git config --global user.name "treystaff"
 git config --global user.email "treystaff@gmail.com"
 
+# Git i3 blocks (keep repo in opt for now...
+apt-get install -y ruby-ronn acpi
+cd /opt/
+git clone git://github.com/vivien/i3blocks
+cd i3blocks
+make clean all
+make install
+
 # Clone relevant repositories to /code/ dir (created before provisioning)
 cd /code/
 git clone https://github.com/treystaff/PhenoAnalysis.git
@@ -40,15 +66,30 @@ apt-get install -y python-numpy python-scipy python-pandas python-gdal python-tk
 pip install -y pytesseract
 pip install -y simplekml
 
-# Finally, Install Pycharm
-cd /tmp/
-wget https://download.jetbrains.com/python/pycharm-community-4.5.3.tar.gz
-tar -xzf pycharm-community-4.5.3.tar.gz
-# Not sure what the best place to put this is, so stick in /opt/...
-mkdir -p /opt/packages/pycharm
-cp -r /tmp/pycharm-community-4.5.3/* /opt/packages/pycharm/
-rm -rf /tmp/pycharm*
-# Symlink startup to /usr/local/bin. Maybe should put installation files here too?
-ln -s /opt/packages/pycharm/bin/pycharm.sh /usr/local/bin/pycharm
-# Start PyCharm
-pycharm
+# Now link dotfiles to the appropriate places. 
+# (Maybe move this bit into its own script later)
+# First move old files to a holding location
+mkdir ~/.old_dotfiles
+dotfiles="bashrc gitconfig vimrc xmodmap i3/config i3/i3blocks.conf"
+for dotfile in $dotfiles; do
+	if [ -f ~/.$dotfile ]
+	then
+		mv ~/.$dotfile ~/.old_dotfiles
+	fi
+	# For now, assume dir is /code/pc_setup/dotfiles.
+	ln -s /code/pc_setup/dotfiles/$dotfile ~/.$dotfile 
+done
+
+# May re-add later...Trying to mostly use Vim
+## Finally, Install Pycharm
+#cd /tmp/
+#wget https://download.jetbrains.com/python/pycharm-community-4.5.3.tar.gz
+#tar -xzf pycharm-community-4.5.3.tar.gz
+## Not sure what the best place to put this is, so stick in /opt/...
+#mkdir -p /opt/packages/pycharm
+#cp -r /tmp/pycharm-community-4.5.3/* /opt/packages/pycharm/
+#rm -rf /tmp/pycharm*
+## Symlink startup to /usr/local/bin. Maybe should put installation files here too?
+#ln -s /opt/packages/pycharm/bin/pycharm.sh /usr/local/bin/pycharm
+## Start PyCharm
+#pycharm
